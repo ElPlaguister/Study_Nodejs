6489aca7ffc06bc110a109fcc5fdfa2be2079d86
Kritias <50093609+ElPlaguister@users.noreply.github.com>
2021-08-30 23:44:36 +0900

    practice avgs, readdir

--- main.js
+++ main.js
@@ -1,59 +1,60 @@
+function load_template(desc, title) {
+    title = title.replace(/_/gi, ' ');
+    console.log('title: ' + title);
+    var template = `
+    <!doctype html>
+    <html>
+        <head>
+            <title>백병전 - ${title}</title>
+            <meta charset="utf-8">
+        </head>
+        <body>
+            <h1><a href="/">백병전</a></h1>
+            <div id="grid">
+                <div>
+                    <ul>
+                        <li><a href="/?id=백병전에서도_총은_필수다">백병전에서.. </a></li>
+                        <li><a href="/?id=역사_및_특징">역사 및 특징</a></li>
+                        <li><a href="/?id=고통">고통</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <h2>${title}</h2>
+                    <article>${desc}</article>
+                </div>
+            </div>
+        </body>
+    </html>
+    `;
+    return template;
+}
+
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
-
 var app = http.createServer(function(request,response){
     var _url = request.url;
+    console.log(url.parse(_url, true));
     var queryData = url.parse(_url, true).query;
-    var title = ''
-    console.log('id: ' + queryData.id);
-    console.log('url:' + _url);
-    if(_url == '/'){
-      title = 'Welcome';
-      console.log('title is Welcome');
-    }
-    else if(_url == '/favicon.ico'){
+    var pathname = url.parse(_url, true).pathname;
+    var title = '';
+    if(pathname == '/'){
+        response.writeHead(200);
+        if(queryData.id == undefined) {
+            title = 'Welcome';
+            desc = 'Hello, Node.js';
+            response.end(load_template(desc, title));
+        }
+        else {
+            title = queryData.id;
+            fs.readFile(`data/${title}`, 'utf-8', function(err, desc) {
+                response.end(load_template(desc, title));
+            });
+        }
+    } 
+    else {
         response.writeHead(404);
-        response.end();
-        return;
+        response.end('Not found');
     }
-    else if(queryData.id != undefined){
-        title = queryData.id;
-    }
-    response.writeHead(200);
-    fs.readFile(`data/${queryData.id}`, 'utf-8', function(err, desc) {
-        title = title.replace(/_/gi, ' ');
-        console.log('title: ' + title);
-        var template = `
-        <!doctype html>
-        <html>
-            <head>
-                <title>백병전 - ${title}</title>
-                <meta charset="utf-8">
-                <link rel="stylesheet" href="style.css">
-                <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-                <script src="darkmode.js"></script>
-            </head>
-            <body>
-                <h1><a href="/">백병전</a></h1>
-                <input type="button" value="dark" onclick="DarkmodeHandler(this)">
-                <div id="grid">
-                    <div>
-                        <ul>
-                            <li><a href="/?id=백병전에서도_총은_필수다">백병전에서.. </a></li>
-                            <li><a href="/?id=역사_및_특징">역사 및 특징</a></li>
-                            <li><a href="/?id=고통">고통</a></li>
-                        </ul>
-                    </div>
-                    <div>
-                        <h2>${title}</h2>
-                        <article>${desc}</article>
-                    </div>
-                </div>
-            </body>
-        </html>
-        `;
-        response.end(template);
-    });
 });
 app.listen(3000);
